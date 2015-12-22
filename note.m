90f59fb08f7380f554b0f554b207ea690ae2c218
gonul14 <gonulozpulat14@gmail.com>
2015-12-22 11:54:04 +0200

    note.m note fonksiyonunu içerir.

--- note.m
+++ note.m
@@ -1,4 +1,9 @@
-function [xx,tt]=note(f,dur) %fonksiyon giriþ deðerinde argüman olarak frekans ve t'nin duracaðý deðerleri fonksiyonda tanýmladým. 
-tt=0:1/(100*f):dur;      %zaman aralýðým 0'dan baþlayýp dur'a kadar 1/(100*f) artýmmla devam edicek.
-xx=sin(2*pi*f*tt); %xx'e sinüs sinyalini atadým.
+function [xx,tt]=note(frekans,vurus)  %note fonksiyonu giriþ olarak frekans 
+Fs=8192; %Fs sabit deðerim
+tt=0:1/Fs:((0.25-(1/Fs))/2);  %0'dan baþlattým 1/Fs kadar arttýrdým.0.25-1/Fs yaptým çünkü 0. elemanýda sayýcak 1 deðer düþürdüm ve 0,24  yaptým.2048 eleman oldu.Boyut eþitlensin diye "(0,25-1/Fs)/2" ikiye böldüm.1024 elemana kadar artýrdým.
+zz=0; %z'yi sýfýrladým.
+   xx=sin(2*pi*frekans*tt); %sinüs sinyalini tanýmladým.
+if length(xx)==length(zz)    %z ve x 'in boyutu eþitse
+       zz=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))]; %zz'yi döndür.
+end
 end
