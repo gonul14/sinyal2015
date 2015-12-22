9b45b154a7b3f39247bd89b353e07b7931a42110
gonul14 <gonulozpulat14@gmail.com>
2015-12-22 11:56:54 +0200

    Update note_cgr.m

--- note_cgr.m
+++ note_cgr.m
@@ -1,15 +1,15 @@
-[x,y]=note(100,1/8)  %frekans ve vurus değeri girildi.
-plot(y,x)           %sinüs sinyali çizdirdim.
+[x,y]=note(100,1/8)  %frekans ve vurus deÄŸeri girildi.
+plot(y,x)           %sinÃ¼s sinyali Ã§izdirdim.
 
-zz1=[linspace(0,1.5,8192*(1/8)*(2/8)),linspace(1.5,1,8192*(1/8)*(1/8)),linspace(1,1,8192*(1/8)*(4/8)),linspace(1,0,8192*(1/8)*(1/8))]  %zz1'de zarfın pozitif kısmını çizdirdiö
-                                                                                                                                       %başlangıç değeri,bitiş değeri,Fs*vurus*t degisimi formülüyele zarfın zz1'de pozitif kısmını çizdirdim.
+zz1=[linspace(0,1.5,8192*(1/8)*(2/8)),linspace(1.5,1,8192*(1/8)*(1/8)),linspace(1,1,8192*(1/8)*(4/8)),linspace(1,0,8192*(1/8)*(1/8))]  %zz1'de zarfÄ±n pozitif kÄ±smÄ±nÄ± Ã§izdirdir
+                                                                                                                                       %baÅŸlangÄ±Ã§ deÄŸeri,bitiÅŸ deÄŸeri,Fs*vurus*t degisimi formÃ¼lÃ¼yele zarfÄ±n zz1'de pozitif kÄ±smÄ±nÄ± Ã§izdirdim.
    plot(y,zz1)
     
-zz2=[linspace(0,-1.5,8192*(1/8)*(2/8)),linspace(-1.5,-1,8192*(1/8)*(1/8)),linspace(-1,-1,8192*(1/8)*(4/8)),linspace(-1,0,8192*(1/8)*(1/8))]  %zz2'de zarfın pozitif kısmını çizdirdiö
-                                                                                                                                            %başlangıç değeri,bitiş değeri,Fs*vurus*t degisimi formülüyele zarfın zz2'de negatif kısmını çizdirdim.
+zz2=[linspace(0,-1.5,8192*(1/8)*(2/8)),linspace(-1.5,-1,8192*(1/8)*(1/8)),linspace(-1,-1,8192*(1/8)*(4/8)),linspace(-1,0,8192*(1/8)*(1/8))]  %zz2'de zarfÄ±n pozitif kÄ±smÄ±nÄ± Ã§izdirdiÃ¶
+                                                                                                                                            %baÅŸlangÄ±Ã§ deÄŸeri,bitiÅŸ deÄŸeri,Fs*vurus*t degisimi formÃ¼lÃ¼yele zarfÄ±n zz2'de negatif kÄ±smÄ±nÄ± Ã§izdirdim.
 plot(y,zz2)
 
-zz=zz1.*zz2 %zz1*zz2'deki tüm elemanları tek tek çarptım ve z3'atadım.
+zz=zz1.*zz2 %zz1*zz2'deki tÃ¼m elemanlarÄ± tek tek Ã§arptÄ±m ve z3'atadÄ±m.
 
-z3=x.*zz  %x'teki her bir elemanı zz3 ile çarptım ve carp'a atadım.
-plot(y,z3) %y'ye göre carp çizdirdim.
+z3=x.*zz  %x'teki her bir elemanÄ± zz3 ile Ã§arptÄ±m ve carp'a atadÄ±m.
+plot(y,z3) %y'ye gÃ¶re carp Ã§izdirdim.
