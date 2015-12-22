3a1aff048a72053097743711e8c160233220f2a5
gonul14 <gonulozpulat14@gmail.com>
2015-12-22 11:57:29 +0200

    Update note_cgr.m

--- note_cgr.m
+++ note_cgr.m
@@ -1,11 +1,11 @@
 [x,y]=note(100,1/8)  %frekans ve vurus değeri girildi.
 plot(y,x)           %sinüs sinyali çizdirdim.
 
-zz1=[linspace(0,1.5,8192*(1/8)*(2/8)),linspace(1.5,1,8192*(1/8)*(1/8)),linspace(1,1,8192*(1/8)*(4/8)),linspace(1,0,8192*(1/8)*(1/8))]  %zz1'de zarfın pozitif kısmını çizdirdir
+zz1=[linspace(0,1.5,8192*(1/8)*(2/8)),linspace(1.5,1,8192*(1/8)*(1/8)),linspace(1,1,8192*(1/8)*(4/8)),linspace(1,0,8192*(1/8)*(1/8))]  %zz1'de zarfın pozitif kısmını çizdirir
                                                                                                                                        %başlangıç değeri,bitiş değeri,Fs*vurus*t degisimi formülüyele zarfın zz1'de pozitif kısmını çizdirdim.
    plot(y,zz1)
     
-zz2=[linspace(0,-1.5,8192*(1/8)*(2/8)),linspace(-1.5,-1,8192*(1/8)*(1/8)),linspace(-1,-1,8192*(1/8)*(4/8)),linspace(-1,0,8192*(1/8)*(1/8))]  %zz2'de zarfın pozitif kısmını çizdirdiö
+zz2=[linspace(0,-1.5,8192*(1/8)*(2/8)),linspace(-1.5,-1,8192*(1/8)*(1/8)),linspace(-1,-1,8192*(1/8)*(4/8)),linspace(-1,0,8192*(1/8)*(1/8))]  %zz2'de zarfın pozitif kısmını çizdirir
                                                                                                                                             %başlangıç değeri,bitiş değeri,Fs*vurus*t degisimi formülüyele zarfın zz2'de negatif kısmını çizdirdim.
 plot(y,zz2)
 
