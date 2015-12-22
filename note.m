a7da5046251d622063670ab8b6937e9e459547dc
gonul14 <gonulozpulat14@gmail.com>
2015-12-22 12:01:27 +0200

    Update note.m

--- note.m
+++ note.m
@@ -1,9 +1,9 @@
-function [xx,tt]=note(frekans,vurus)  %note fonksiyonu giriş olarak frekans 
-Fs=8192; %Fs sabit değerim
-tt=0:1/Fs:((0.25-(1/Fs))/2);  %0'dan başlattım 1/Fs kadar arttırdım.0.25-1/Fs yaptım çünkü 0. elemanıda sayıcak 1 değer düşürdüm ve 0,24  yaptım.2048 eleman oldu.Boyut eşitlensin diye "(0,25-1/Fs)/2" ikiye böldüm.1024 elemana kadar artırdım.
-zz=0; %z'yi sıfırladım.
-   xx=sin(2*pi*frekans*tt); %sinüs sinyalini tanımladım.
-if length(xx)==length(zz)    %z ve x 'in boyutu eşitse
-       zz=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))]; %zz'yi döndür.
+function [xx,tt]=note(frekans,vurus)  %note fonksiyonu giriÅŸ olarak frekans ve vurus deÄŸerleri alÄ±r.
+Fs=8192; %Fs sabit deÄŸerim
+tt=0:1/Fs:((0.25-(1/Fs))/2);  %0'dan baÅŸlattÄ±m 1/Fs kadar arttÄ±rdÄ±m.0.25-1/Fs yaptÄ±m Ã§Ã¼nkÃ¼ 0. elemanÄ±da sayÄ±cak 1 deÄŸer dÃ¼ÅŸÃ¼rdÃ¼m ve 0,24  yaptÄ±m.2048 eleman oldu.Boyut eÅŸitlensin diye "(0,25-1/Fs)/2" ikiye bÃ¶ldÃ¼m.1024 elemana kadar artÄ±rdÄ±m.
+zz=0; %z'yi sÄ±fÄ±rladÄ±m.
+   xx=sin(2*pi*frekans*tt); %sinÃ¼s sinyalini tanÄ±mladÄ±m.
+if length(xx)==length(zz)    %z ve x 'in boyutu eÅŸitse
+       zz=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))]; %zz'yi dÃ¶ndÃ¼r.
 end
 end
